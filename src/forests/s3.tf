570a109f10e76cbf5963444bfe153381e796b119
Guy Sulema <48174831+guy-suli@users.noreply.github.com>
2022-06-15 10:11:01 +0300

    sadsadsadsad

--- src/forests/s3.tf
+++ src/forests/s3.tf
@@ -52,6 +52,21 @@ resource "aws_s3_bucket" "operations" {
 
 }
 
+resource "aws_s3_bucket" "datasadsadsadsadsadsadsadsa" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
+
 resource "aws_s3_bucket" "data_science" {
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
